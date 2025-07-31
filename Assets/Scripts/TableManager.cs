bc731c413c0dc8aad2417d6432ca48d50f4e4db5
leeSEOyoung <seosuper01@snu.ac.kr>
2025-07-31 20:10:12 +0900

    [feat] Cat 생성 기능 추가

--- Assets/Scripts/TableManager.cs
+++ Assets/Scripts/TableManager.cs
@@ -3,19 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class railManager : MonoBehaviour
+public class TableManager : MonoBehaviour
 {
     [Header("Rail")]
+    [SerializeField] private int railCnt;
     [SerializeField] private GameObject railPref, railParent;
-    [SerializeField] private int railCnt, servingCnt;
     [SerializeField] private Sprite[] railSprites;
     
-    [Header("Dish")]
+    [Header("Serving")]
+    [SerializeField] private int servingCnt;
     [SerializeField] private GameObject dishPref, dishParent;
+    
+    [Header("Dining")]
     [SerializeField] private int diningCnt;
+    [SerializeField] private GameObject catPref, catParent;
 
-    private readonly float _posY = 1.35f, _posXFactor = 1.8f;
-    private float _startPosX;
+    private readonly float _railPosY = 1.35f, _catPosY = 3f, _posXFactor = 1.8f;
+    private float _rotationMinPosX, _diningMinPosX, _servingMinPosX;
 
     private void Awake()
     {
@@ -25,25 +29,26 @@ public class railManager : MonoBehaviour
     private void Start()
     {
         // railCnt = GameManager.Instance.railCnt;
-        Initrail();
+        InitTable();
     }
 
-    public void Initrail()
+    public void InitTable()
     {
         if (railCnt < 1 || servingCnt < 1 || diningCnt < 1) { Debug.LogError($"something's wrong [railCnt == {railCnt}, servingCnt == {servingCnt}, diningCnt == {diningCnt}]"); return; }
         if (servingCnt > railCnt) { Debug.LogError($"servingCnt is bigger than railCnt [railCnt == {railCnt}, servingCnt == {servingCnt}]"); return; }
         if (diningCnt > railCnt) { Debug.LogError($"diningCnt is bigger than railCnt [railCnt == {railCnt}, diningCnt == {diningCnt}]"); return; }
         
-        Generaterail();
+        GenerateRail();
         GenerateDish();
+        GenerateCat();
     }
 
-    private void Generaterail()
+    private void GenerateRail()
     {
         float currPosX = -1f * (float)(railCnt / 2) * _posXFactor;
         currPosX -= railCnt % 2 == 1 ? _posXFactor : 0.5f * _posXFactor;
         
-        _startPosX = currPosX + _posXFactor;
+        _rotationMinPosX = currPosX;
 
         GameObject tmpObj;
         
@@ -63,7 +68,7 @@ public class railManager : MonoBehaviour
                 else
                 {
                     tmpObj = Instantiate(railPref, railParent.transform);
-                    tmpObj.transform.localPosition = new Vector3(currPosX, _posY * j, 0);
+                    tmpObj.transform.localPosition = new Vector3(currPosX, _railPosY * j, 0);
                     if (i == -1 || i == railCnt)
                     {
                         SpriteRenderer spr = tmpObj.GetComponent<SpriteRenderer>();
@@ -84,16 +89,37 @@ public class railManager : MonoBehaviour
     private void GenerateDish()
     {
         float currPosX = -1f * (float)(servingCnt / 2) * _posXFactor;
-        // currPosX -= railCnt % 2 == 1 ? _posXFactor : 0.5f * _posXFactor;
+        _servingMinPosX = currPosX;
         
         GameObject tmpObj;
 
         for (int i = 0; i < servingCnt; i++)
         {
             tmpObj = Instantiate(dishPref, dishParent.transform);
-            tmpObj.transform.localPosition = new Vector3(currPosX, _posY * -1f, 0);
+            tmpObj.transform.localPosition = new Vector3(currPosX, _railPosY * -1f, 0);
             // tmpObj.GetComponent<SpriteRenderer>().sprite = ;
             currPosX += _posXFactor;
         }
+    }
+
+    private void GenerateCat()
+    {
+        float currPosX = -1f * (float)(diningCnt / 2) * _posXFactor;
+        _diningMinPosX = currPosX;
+        
+        GameObject tmpObj;
+
+        for (int i = 0; i < diningCnt; i++)
+        {
+            tmpObj = Instantiate(catPref, catParent.transform);
+            tmpObj.transform.localPosition = new Vector3(currPosX, _catPosY, 0);
+            // tmpObj.GetComponent<SpriteRenderer>().sprite = ;
+            currPosX += _posXFactor;
+        }
+    }
+
+    public void RotateDish()
+    {
+        
     }
 }
