ab36fe6a94957e109a6affbb6de434cca60faf25
sonojae <sson41541241@gmail.com>
2025-08-03 08:39:19 +0900

    [fix] TooltipUpdate

--- Assets/Scripts/TooltipManager.cs
+++ Assets/Scripts/TooltipManager.cs
@@ -16,25 +16,25 @@ public class TooltipManager : MonoBehaviour
     // Start is called before the first frame update
     private Dictionary<ResultTypes, string> resulTypeTooltip = new Dictionary<ResultTypes, string>
     {
-        { ResultTypes.GenerateCard1, "Generate Card 1" },
-        { ResultTypes.GenerateCard2, "Generate Card 2" },
-        { ResultTypes.GenerateSushi, "Generate Sushi" },
-        { ResultTypes.GiveTip, "Give Tip" },
-        { ResultTypes.EmptyNextDish, "Empty Next Dish" },
-        { ResultTypes.EmptyColorDish, "Empty Color Dish" },
-        { ResultTypes.GenerateSushiOnColorDish, "Generate Sushi on Color Dish" },
-        { ResultTypes.ChangeType, "Change Type" },
-        { ResultTypes.ChangeCard, "Change Card" }
+        { ResultTypes.GenerateCard1, "Gain this card." },
+        { ResultTypes.GenerateCard2, "Gain two copies of this card." },
+        { ResultTypes.GenerateSushi, "Generate this sushi on the first empty plate." },
+        { ResultTypes.GiveTip, "Pay double the price." },
+        { ResultTypes.EmptyNextDish, "Turn the back plate into an empty plate." },
+        { ResultTypes.EmptyColorDish, "Turn a plate of this color into an empty plate." },
+        { ResultTypes.GenerateSushiOnColorDish, "Generate this sushi on a plate of this color." },
+        { ResultTypes.ChangeType, "Turn the first sushi plate into the second sushi plate." },
+        { ResultTypes.ChangeCard, "Turn the first card into the second card." }
     };
     
     private Dictionary<ConditionTypes, string> conditionTooltip = new Dictionary<ConditionTypes, string>
     {
-        { ConditionTypes.SushiEaten, "Sushi Eaten" },
-        { ConditionTypes.CardPlaced, "Card Placed" },
-        { ConditionTypes.CardGenerated, "Card Generated" },
-        { ConditionTypes.SushiGenerated, "Sushi Generated" },
-        { ConditionTypes.SushiPassed, "Sushi Passed" },
-        { ConditionTypes.DishPassed, "Dish Passed" }
+        { ConditionTypes.SushiEaten, "If the cat eats this sushi…" },
+        { ConditionTypes.CardPlaced, "If this card is used…" },
+        { ConditionTypes.CardGenerated, "If this card is gained…" },
+        { ConditionTypes.SushiGenerated, "If this sushi is generated…" },
+        { ConditionTypes.SushiPassed, "If this sushi/empty plate passes by the cat…" },
+        { ConditionTypes.DishPassed, "If a plate of this color passes by the cat…" }
     };
     
     
